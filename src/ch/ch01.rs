d4c122ff49b8e11f547817a0001d354419e78191
p5chmitz <petermschmitz@gmail.com>
2024-08-08 18:52:04 -0700

    Smol edits for project integrity

--- src/ch/ch01.rs
+++ src/ch/ch01.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 
-use std::task::Wake;
-
 /**
  * This is a sandbox crate for chapter 1 of Data Structures and Algorithm Analysis in Java by Mark Allen Weiss
  */
@@ -52,95 +50,4 @@ pub fn binary_search_test() {
         None => 0,
     };
     assert_eq!(result, 37)
-}
-
-// Cheating with GPT
-//pub fn unique_0(a: &Vec<i32>) -> bool {
-//    for j in 0..a.len() {
-//        for k in j + 1..a.len() {
-//            if a[j] == a[k] {
-//                println!("{} appears more than once", a[j]);
-//                return false;
-//            }
-//        }
-//    }
-//    true
-//}
-
-// Checks a single array for uniqueness in O(n^2) time
-pub fn unique_1(a: &Vec<i32>) -> bool {
-    for (j, val) in a.iter().enumerate() {
-        let start = (j + 1) as usize;
-        if start <= a.len() {
-            for k in &a[start ..] {
-                if val == k {
-                    println!("{val} appears more than once");
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-
-// Checks an array for uniqueness in O(n * log(n)) time
-pub fn unique_3(a: &Vec<i32>) -> bool {
-    a.to_owned().sort();
-    for j in 0..a.len() {
-        if (j + 1) < a.len() && a[j] == a[j + 1] {
-            println!("Found one! {}", a[j]);
-            return false;
-        }
-    }
-    true
-}
-
-// Same as 3 but more elegant (cheating with GPT)
-pub fn unique_4(a: &Vec<i32>) -> bool {
-    a.to_owned().sort();
-    for j in 0..a.len() - 1 {
-        if a[j] == a[j + 1] {
-            println!("Found a duplicate: {}", a[j]);
-            return false;
-        }
-    }
-    true
-}
-
-pub fn unique_2(a: &Vec<i32>, b: &Vec<i32>) -> bool {
-    for j in a.iter() {
-        for k in b.iter() {
-            if *k == *j {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-/** Calculates a prefix average of an array in O(n^2) time */
-pub fn prefix_average_0(a: &Vec<f32>) -> Vec<f32> {
-    let mut avg: Vec<f32> = a.clone();
-    // i is the index, and a[i] is the value at index i
-    for j in 0..a.len() {
-        let mut total: f32 = 0.0;
-        for k in 0..=j as usize {
-            total += a[k];
-        }
-        avg[j] = total / (j as f32 + 1.0);
-        //println!("t: {}, i: {}", total, j)
-    }
-    avg
-}
-
-/** Calculates a prefix average of an array in O(n) time */
-pub fn prefix_average_1(a: &Vec<f32>) -> Vec<f32> {
-    let mut avg: Vec<f32> = a.clone();
-    let mut total: f32 = 0.0;
-    // i is the index, and a[i] is the value at index i
-    for j in 0..a.len() {
-        total += a[j];
-        avg[j] = total / (j as f32 + 1.0);
-    }
-    avg
 }
