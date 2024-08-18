8da3bd791edf6fb28ba577565211b552f196057e
p5chmitz <petermschmitz@gmail.com>
2024-08-18 00:08:30 -0700

    Adds some correct implementations to tgg_05
    - Correct iterative and recursive implementations of the
    array reversal algorithm
    - Adds some details about using iterators vs ranges properly

--- src/tgg/tgg_05.rs
+++ src/tgg/tgg_05.rs
@@ -104,6 +104,49 @@ pub fn disk_usage(root: &Path) -> u64 {
     return dir_size;
 }
 
+pub fn test() {
+    // Vector v has 5 elements with indexes 0 - 4
+    let v: Vec<i32> = vec![10, 20, 30, 40, 50];
+    let magic_num: i32 = 50;
+
+
+    // Standard for loop with STANDARD range
+    // Note that using inclusive range ..= results in overflow
+    for i in 0..v.len() {
+        print!("{}, ", v[i]);
+        if v[i] == magic_num {
+            println!("End");
+        }
+    }
+
+    // De-sugared for loop over iterator expression as a range
+    let mut iter: std::ops::Range<usize> = (0..v.len()).into_iter();
+    while let Some(i) = iter.next() {
+        print!("{}, ", v[i]);
+        if v[i] == magic_num {
+            println!("End");
+        }
+    }
+
+    // Requires value de-referencing because iter() yields
+    // index references as &i32
+    for i in v.iter() {
+        print!("{}, ", i);
+        if *i == magic_num {
+            println!("End");
+        }
+    }
+
+    // Alternatively into_iter() takes ownership and 
+    // allows direct access to indexed values
+    for i in v.into_iter() {
+        print!("{}, ", i);
+        if i == magic_num {
+            println!("End");
+        }
+    }
+}
+
 // Sum of array of integers to n indexes 
 // Iterative implementation (so easy, so intuitive)
 pub fn array_sum_0(v: Vec<i32>) -> i32 {
@@ -113,12 +156,20 @@ pub fn array_sum_0(v: Vec<i32>) -> i32 {
     }
     sum
 }
+// Reimplementation using an iterator instead of a range
+pub fn array_sum_1(v: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for e in v.iter() {
+        sum += v[*e as usize]
+    }
+    sum
+}
 // Recursive implementation (is dumb)
-pub fn array_sum_1(data: &Vec<i32>, n: i32) -> i32 {
+pub fn array_sum_2(data: &Vec<i32>, n: i32) -> i32 {
     if n == 0 {
         return 0;
     } else {
-        return array_sum_1(&data, n - 1) + &data[n as usize - 1];
+        return array_sum_2(&data, n - 1) + &data[n as usize - 1];
     }
 }
 #[test]
@@ -129,41 +180,45 @@ pub fn array_sum_test() {
     // Recursive implementation test
     // Dumb, error prone implementation
     let t = vec![1, 2, 3, 4, 5, 6];
-    assert_eq!(21, array_sum_1(&t, 6));
+    assert_eq!(21, array_sum_2(&t, 6));
 }
 
 // Iteratively reverses the elements of an array 
+/** Iteratively reverses the elements of an array in O(n) time */
 pub fn array_reversal_0(mut v: Vec<i32>) -> Vec<i32> {
-    let mut high = v.len();
+    let mut high = v.len() - 1; // Match the index values, not # of elements
+    let mut low  = 0;
     let mut temp;
-    println!("Original: {:?}", v);
-    for e in 0..v.len() - 1 {
-        if high > e {
-            temp = v[e];
-            v[e] = high as i32;
-            v[high - 1] = temp; 
-            high -= 1;
-        }
+    println!("Iterative approach\nOriginal: {:?}", v);
+    while high > low {
+        temp = v[low];
+        v[low] = v[high];
+        v[high] = temp;
+        high -= 1;
+        low += 1;
     }
+    println!("Reversed: {:?}", v);
     return v;
 }
 // Recursively reverses the elements of an array in place with liberal type conversion
-pub fn array_reversal_1(v: &mut Vec<i32>, low: i32, high: i32) {
+pub fn array_reversal_1(v: &mut Vec<i32>, low: i32, high: i32) -> &mut Vec<i32> {
     if low < high {
         let temp = v[low as usize];
-        v[low as usize] = high as i32;
-        v[high as usize - 1] = temp; 
+        v[low as usize] = v[high as usize];
+        v[high as usize] = temp; 
         array_reversal_1(v, low + 1, high - 1);
     }
+    v
 }
-// Recursive reimplementation using native usize for cleaner presentation
-pub fn array_reversal_2(v: &mut Vec<usize>, low: usize, high: usize) {
+// Recursive reimplementation but everything is usize
+pub fn array_reversal_2(v: &mut Vec<usize>, low: usize, high: usize) -> &mut Vec<usize> {
     if low < high {
         let temp = v[low];
-        v[low] = high;
-        v[high - 1] = temp; 
+        v[low] = v[high];
+        v[high] = temp; 
         array_reversal_2(v, low + 1, high - 1);
     }
+    v
 }
 #[test]
 pub fn array_reversal_test() {
@@ -173,9 +228,9 @@ pub fn array_reversal_test() {
     assert_eq!(array_reversal_0(v), rev);
 
     // Tests the dumb recursive approach
-    let mut v = vec![1, 2, 3, 4, 5, 6, 7, 8];
-    let rev = vec![8, 7, 6, 5, 4, 3, 2, 1];
-    let high = v.len() as i32;
+    let mut v = vec![11, 22, 33, 44, 55, 66, 77, 88];
+    let rev = vec![88, 77, 66, 55, 44, 33, 22, 11];
+    let high = v.len() as i32 - 1;
     array_reversal_1(&mut v, 0, high);
     assert_eq!(v, rev) 
 }
@@ -183,32 +238,8 @@ pub fn array_reversal_test() {
 // EXTRA CREDIT
 ///////////////
 
-// Tower of Hanoi problem
-pub fn hanoi_0(n: i32) {
-    let mut a: Vec<i32> = Vec::new();
-    let mut b: Vec<i32> = Vec::new();
-    let mut c: Vec<i32> = Vec::new();
-    
-    for e in (0..=n).rev() {
-        a.push(e + 1)
-    }
-
-    for _ in (0..=a.len()).rev() {
-        b.push(a.pop().expect("Ah beans"));
-        println!("a: {:?}\nb: {:?}\nc: {:?}\n-----", &a, &b, &c);
-        // Move the ring from peg a to peg c
-        c.push(a.pop().expect("Ah beans"));
-        println!("a: {:?}\nb: {:?}\nc: {:?}\n-----", &a, &b, &c);
-        // Move the ring from peg b to peg c
-        c.push(b.pop().expect("Ah beans"));
-        println!("a: {:?}\nb: {:?}\nc: {:?}\n-----", &a, &b, &c);
-        // Move the ring from peg c to peg b
-        // Move the ring from peg c to peg a
-        // Move the ring from peg b to peg c
-    }
-}
-// The internet's version of the recursive algorithm
-/** A recursive implementation of the Tower of Hanoi problem that
+// The internet's version of the recursive solution
+/** A recursive implementation of the Tower of Hanoi solution that
  * runs in O(2^n) time. This algorithm works by breaking the
  * problem set into source, auxiliary, and destination pegs. */
 pub fn tower_of_hanoi(n: u32, src: char, dest: char, aux: char) {
@@ -216,14 +247,8 @@ pub fn tower_of_hanoi(n: u32, src: char, dest: char, aux: char) {
         println!("Move disk 1 from peg {} to peg {}", src, dest);
         return;
     }
-    
-    // Step 1: Move n-1 disks from src to aux peg
     tower_of_hanoi(n - 1, src, aux, dest);
-    
-    // Step 2: Move the nth disk from src to dest peg
-    println!("Move disk {} from peg {} to peg {}", n, src, dest);
-    
-    // Step 3: Move n-1 disks from aux peg to dest peg
+    println!("Move disk {} from peg {} to peg {}", n, src, dest); // Trace
     tower_of_hanoi(n - 1, aux, dest, src);
 }
 
