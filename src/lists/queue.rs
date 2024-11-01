2abd1a2db9c30caefa6ecbcc6ef11a72106a9d80
p5chmitz <petermschmitz@gmail.com>
2024-10-31 23:28:00 -0700

    Major edit to queues

--- src/lists/queue.rs
+++ src/lists/queue.rs
@@ -77,12 +77,21 @@ pub mod circular_queue {
         size: usize,
         capacity: usize,
     }
-
+    /** The CircularQueue's public API contains the following functions: 
+     * - new(capacity: usize) -> CircularQueue<T>
+     * - enqueue(&mut self, item: T) -> Result<(), &str>
+     * - dequeue(&mut self) -> Option<T>
+     * NOTE: All functions operation in O(1) time */
     impl<T> CircularQueue<T> {
-        /** Creates a queue that contains `capacity` number of elements */
+        /** Creates a queue that contains `capacity` number of elements in O(1) time */
         pub fn new(capacity: usize) -> CircularQueue<T> {
             let mut data = Vec::with_capacity(capacity);
-            data.resize_with(capacity, || None); // Fills `data` with `None`
+            // Fills the vec with None
+            data.resize_with(capacity, Default::default); // The most efficient way
+            //data.resize_with(capacity, || None); // More explicit, but less efficient
+            //for _ in 0..capacity { // 4x slower than resize_with using Default
+            //    data.push(None)
+            //}
             CircularQueue {
                 data,
                 front: 0,
@@ -91,20 +100,19 @@ pub mod circular_queue {
                 capacity,
             }
         }
-        /** Adds an element to the back of the queue */
+        /** Adds an element to the back of the queue in O(1) time */
         pub fn enqueue(&mut self, item: T) -> Result<(), &str> {
             // Ensures that the queue cannot take more elements than its capacity
             if self.size == self.capacity {
                 return Err("Queue is full");
             }
-            // Writes the value to the proper position
+            // Calculates the next available positionm, writes to it, and increases size
+            self.back = (self.front + self.size) % self.capacity;
             self.data[self.back] = Some(item);
-            // Properly advances the back index with wrapping
-            self.back = (self.back + 1) % self.capacity;
             self.size += 1;
             Ok(())
         }
-        /** Removes and returns the front element of the queue */
+        /** Removes and returns the front element of the queue in O(1) time */
         pub fn dequeue(&mut self) -> Option<T> {
             // Checks if queue is empty and returns proper None
             if self.size == 0 {
@@ -122,38 +130,42 @@ pub mod circular_queue {
     #[test]
     fn circular_queue_test() {
         let mut q: CircularQueue<char> = CircularQueue::new(3);
-        let mut remaining = 0;
         // All these shits panic because pushing and popping a full/empty queue is an error
-        q.enqueue('a').unwrap();
-        q.enqueue('b').unwrap();
-        q.enqueue('c').unwrap();
+        q.enqueue('a').unwrap(); 
+        q.enqueue('b').unwrap(); 
+        q.enqueue('c').unwrap(); 
 
-        assert_eq!(remaining, q.capacity - q.size);
-        assert_eq!(q.size, q.capacity); // Test that the queue is now at capacity
-        assert_eq!(q.back, 0); // Test that the next available position has wrapped
+        assert_eq!(0, q.capacity - q.size); // The number of empty indexes
+        assert_eq!(q.size, q.capacity); // That the queue is now at capacity
+        assert_eq!(q.front, 0); // The front is still zero                                
+        assert_eq!(q.back, 2); // The last operation wrote to the last available position
+
+        assert!(q.enqueue('d').is_err()); // That the queue cant take additional elements
 
         let a = q.dequeue().unwrap();
-        assert_eq!(a, 'a');
+        assert_eq!(a, 'a'); // Tests that the dequeue pulls from the front of the queue
         assert_eq!(q.size, 2); // Test that the size is being appropriately decremented too
-
-        // Push the queue back to capacity and ensure it cant take additional elements
-        q.enqueue('d').unwrap();
-        assert!(q.enqueue('e').is_err());
+        assert_eq!(1, q.capacity - q.size); // Tests that dequeue is "freeing" indexes
+        assert_eq!(q.front, 1); // The front advanced by one with dequeue                               
+        assert_eq!(q.back, 2); // The back is still 2
 
         // Test that the dequeue is indeed pulling from the front
         // and that the size remains accurate
-        q.dequeue().unwrap(); // b
-        remaining = 1;
-        assert_eq!(remaining, q.capacity - q.size);
-        q.dequeue().unwrap(); // c
+        q.dequeue().unwrap(); 
+        q.dequeue().unwrap(); 
+        q.enqueue('d').unwrap(); // That the queue can take additional elements again
         let d = q.dequeue().unwrap();
-        assert_eq!(d, 'd');
+        assert_eq!(d, 'd'); // And that they're correct
 
-        // Test that the back has wrapped all the way around past the start
-        // and that there are no more elements in the queue; your work is done!
-        assert_eq!(q.back, 1);
+        // Final state of the queue
+        assert_eq!(q.front, 1);
+        assert_eq!(q.back, 0);
         assert_eq!(q.size, 0);
     }
+}
+/** Illustrates a Josephus Problem solution */
+pub fn circular_queue_example() {
+
 }
 
 //////////////////////////////////
@@ -188,6 +200,8 @@ pub mod linked_queue {
                 length: 0,
             }
         }
+        /** Runs in O(n) because I cant (or dont want to try to) safely 
+         * store a reference to the last node in the list */
         pub fn enqueue(&mut self, frame: Frame<T>) {
             let new_node = Box::new(frame);
         
@@ -207,6 +221,7 @@ pub mod linked_queue {
             }
             self.length += 1;
         }
+        /** Returns a reference to the front of the list in O(1) time */
         pub fn peek(&self) -> Option<&T> {
             //if let Some(s) = &(*self).head {
             //    Some(&s.data)
@@ -215,6 +230,7 @@ pub mod linked_queue {
             //}
             self.front.as_ref().map(|node| &node.data)
         }
+        /** Returns the front of the list (if any) in O(1) time */
         pub fn dequeue(&mut self) -> Option<Frame<T>> {
             if let Some(mut boxed_frame) = self.front.take() {
                 self.front = boxed_frame.next.take();
