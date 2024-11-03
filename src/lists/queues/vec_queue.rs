a90ef316fce381e279a2fb2a514007d022726837
p5chmitz <petermschmitz@gmail.com>
2024-11-03 01:26:53 -0800

    Updates the queue wrapper modules
    - Makes the wrappers generic over T
    - Adds std lib implementation tests to illustrate that you dont need wrappers
    - Adds API lists and running time notes

--- src/lists/queues/vec_queue.rs
+++ src/lists/queues/vec_queue.rs
@@ -3,35 +3,43 @@
 ///////////////////////////
 
 pub mod vec_wrapper {
+    /** The Queue's API contains the following functions: 
+    - new() -> Queue<T>
+    - push(&mut self, s: T)
+    - peek(&self) -> Option<&T>
+    - remove(&mut self) -> Option<T>
+    NOTE: All methods run in O(1) time except remove() which runs in O(n). For more efficient operations
+    use the VecDeque type. */
     #[derive(Debug, PartialEq)]
-    pub struct Queue {
-        data: Vec<char>,
+    pub struct Queue<T> {
+        data: Vec<T>,
         size: usize,
     }
-    impl Queue {
-        fn new() -> Queue {
+    impl<T> Queue<T> {
+        /** Creates a new Queue with an initial capacity of 10 in O(1) time */
+        pub fn new() -> Queue<T> {
             Queue {
                 data: Vec::with_capacity(10),
                 size: 0,
             }
         }
-        /** Adds an element to the queue */
-        fn push(&mut self, s: char) {
+        /** Adds an element to the queue; Amortized to O(1) time */
+        pub fn push(&mut self, s: T) {
             self.size += 1;
             self.data.push(s) // Uses push() from Vec
         }
-        /** Peeks at the top of the list without deleting the element */
-        fn peek(&self) -> Option<&char> {
+        /** Peeks at the top of the list without deleting the element in O(1) time */
+        pub fn peek(&self) -> Option<&T> {
             self.data.last()
         }
-        /** Returns and deletes the top of the queue */
-        fn remove(&mut self) -> Option<char> {
+        /** Returns and deletes the top of the queue in O(n) time */
+        pub fn remove(&mut self) -> Option<T> {
             self.size -= 1;
             Some(self.data.remove(0))
         }
     }
     // Convenience (declarative) macro for building queue! objects like vec!
-    // Necessitates explicit allow attribute to suppress warnings because the macro is only used in tests
+    // Requires explicit allow attribute to suppress warnings because the macro is only used in tests
     #[allow(unused_macros)]
     macro_rules! queue {
         ($($c:expr),*) => {
@@ -53,13 +61,36 @@ pub mod vec_wrapper {
         queue.push('b');
         queue.push('c');
         queue.push('d');
-        let first: Queue = queue!('a', 'b', 'c', 'd');
+        let first: Queue<char> = queue!('a', 'b', 'c', 'd');
         assert_eq!(first, queue);
 
         queue.remove();
         queue.remove();
         queue.push('z');
-        let second = queue!('c', 'd', 'z');
+        let second: Queue<char> = queue!('c', 'd', 'z');
+        assert_eq!(second, queue);
+    }
+}
+
+/** Illustrates how Vec can be used as a queue by itself; Convenient, but remove(0) runs in O(n) time;
+ Use VecDeque for flexible operations in O(1) time */
+pub mod vec {
+    #[test]
+    // Illustrates how Vec can be used as a queue, but with some inefficiencies
+    fn vec_queue_test() {
+        let mut queue = Vec::new();
+        queue.push('a');
+        queue.push('b');
+        queue.push('c');
+        queue.push('d');
+        let first: Vec<char> = vec!('a', 'b', 'c', 'd');
+        assert_eq!(first, queue);
+
+        // remove() scales in O(n) time
+        queue.remove(0);
+        queue.remove(0);
+        queue.push('z');
+        let second: Vec<char> = vec!('c', 'd', 'z');
         assert_eq!(second, queue);
     }
 }
--- src/lists/queues/vecdeque_queue.rs
+++ src/lists/queues/vecdeque_queue.rs
@@ -2,34 +2,86 @@
 /** A VecDeque-based queue */
 /////////////////////////////
 
+/** VecDeque is a more efficient implementation than Vec */
 mod vecdeque_wrapper {
-    // VecDeque is a more efficient implementation than Vec
     use std::collections::VecDeque;
 
-    pub struct Queue {
-        data: VecDeque<char>,
+    /** The Queue's API contains the following functions: 
+    - new() -> Queue<T>
+    - push(&mut self, s: T)
+    - peek(&self) -> Option<&T>
+    - dequeue(&mut self) -> Option<T>
+    NOTE: All methods run in O(1) time */
+    pub struct Queue<T> {
+        data: VecDeque<T>,
         size: usize,
     }
-    impl Queue {
-        fn new() -> Queue {
+    impl<T> Queue<T> {
+        fn new() -> Queue<T> {
             Queue {
                 data: VecDeque::new(),
                 size: 0,
             }
         }
         /** Adds an element to the queue */
-        fn push(&mut self, s: char) {
+        fn push(&mut self, e: T) {
             self.size += 1;
-            self.data.push_back(s) // Uses push() from Vec
+            self.data.push_back(e)
         }
         /** Peeks at the top of the list without deleting the element */
-        fn peek(&self) -> Option<&char> {
+        fn peek(&self) -> Option<&T> {
             self.data.front()
         }
         /** Returns and deletes the top of the queue */
-        fn dequeue(&mut self) -> Option<char> {
+        fn dequeue(&mut self) -> Option<T> {
             self.size -= 1;
             self.data.pop_front()
         }
+    }
+}
+
+/** Illustrates how VecDeque can be used as a queue without a silly wrapper */
+pub mod vec_deque {
+
+    use std::collections::VecDeque;
+
+    #[test]
+    // First forwards
+    fn vecdeque_queue_test_forwards() {
+        let mut queue = VecDeque::new();
+        queue.push_back('a');
+        queue.push_back('b');
+        queue.push_back('c');
+        queue.push_back('d');
+        let first: Vec<char> = vec!('a', 'b', 'c', 'd');
+        let first_vec_deque: VecDeque<char> = VecDeque::from(first);
+        assert_eq!(first_vec_deque, queue);
+
+        queue.pop_front();
+        queue.pop_front();
+        queue.push_back('z');
+        let second: Vec<char> = vec!('c', 'd', 'z');
+        let second_vec_deque: VecDeque<char> = VecDeque::from(second);
+        assert_eq!(second_vec_deque, queue);
+    }
+
+    #[test]
+    // And then backwards
+    fn vecdeque_queue_test_backwards() {
+        let mut queue = VecDeque::new();
+        queue.push_front('a');
+        queue.push_front('b');
+        queue.push_front('c');
+        queue.push_front('d');
+        let first: Vec<char> = vec!('d', 'c', 'b', 'a');
+        let first_vec_deque: VecDeque<char> = VecDeque::from(first);
+        assert_eq!(first_vec_deque, queue);
+
+        queue.pop_back();
+        queue.pop_back();
+        queue.push_front('z');
+        let second: Vec<char> = vec!('z', 'd', 'c');
+        let second_vec_deque: VecDeque<char> = VecDeque::from(second);
+        assert_eq!(second_vec_deque, queue);
     }
 }
