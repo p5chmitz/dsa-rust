ffb66adaa209dce60d62197bd0f9175ada914dca
p5chmitz <petermschmitz@gmail.com>
2025-03-12 22:42:16 -0700

    Adds split and splice with some rudimentary tests

--- src/lists/generic_doubly_linked_list.rs
+++ src/lists/generic_doubly_linked_list.rs
@@ -55,6 +55,7 @@ All lists contain the following non-public utilities
 
 Positional List (Sorted List Support)
 */
+#[derive(Debug)]
 pub struct List<T> {
     head: Link<T>,
     tail: Link<T>,
@@ -131,7 +132,7 @@ impl<T> List<T> {
 
             // If there are already elements in the list...
             if let Some(node) = self.tail {
-                println!("Inserts new tail");
+                //println!("Inserts new tail");
                 // Sets the new node's prev pointer to the current tail
                 (*new_node_wrapper).prev = self.tail;
                 // Sets the original tail's next pointer to the new node
@@ -140,7 +141,7 @@ impl<T> List<T> {
 
             // Inserts into empty list
             else {
-                println!("Inserts at tail");
+                //println!("Inserts at tail");
                 // Sets the list's head and tail pointers to the new node
                 self.head = Some(new_node_wrapper);
             }
@@ -213,12 +214,6 @@ impl<T> List<T> {
 
     //iter_rev() -> Iter
     
-    // Positional list utilities
-    ////////////////////////////
-
-    //pub fn next() -> Cursor<&T>
-    //pub fn peek_next() -> &T {}
-    //pub fn peek_prev() -> &T {}
     //pub fn contains(&T) -> bool {}
     //pub fn find(&T) -> Link<T> {}
 
@@ -376,11 +371,258 @@ impl<'a, T> CursorMut<'a, T> {
         }
     }
 
-    //pub fn split_before(&mut self) -> List<T> {}
-    //pub fn split_after(&mut self) -> List<T> {}
-    //pub fn splice_before(&mut self, mut input: List<T>) {}
-    //pub fn splice_after(&mut self, mut input: List<T>) {}
+    //pub fn remove_current() -> T {}
 
+    /** Returns a new list containint all nodes before the cursor,
+    modifying self to become all nodes after (and including) the cursor */
+    pub fn split_before(&mut self) -> List<T> {
+        // Precondition:
+        //
+        //     list.front -> A <-> B <-> C <-> D <- list.back
+        //                               ^
+        //                              cur
+        //
+        // Postcondition:
+        //
+        //     list.front -> C <-> D <- list.back
+        //                   ^
+        //                  cur
+        //
+        //    return.front -> A <-> B <- return.back
+        //
+
+        // If the cursor is on an element of a non-empty list
+        if let Some(node_ptr) = self.cursor {
+            unsafe {
+                // Captures current state of self
+                let old_len = self.list.len;
+                let old_index = self.index.unwrap();
+                let prev = (*node_ptr).prev;
+
+                // What self will become
+                let new_len = old_len - old_index;
+                let new_head = self.cursor;
+                let new_index = Some(0);
+
+                // What the output will become
+                let output_len = old_len - new_len;
+                let output_head = self.list.head;
+                let output_tail = prev;
+
+                // If the cursor is NOT at a sentinel node,
+                // break pointer links at split:
+                // - cursor_next is the new list's head
+                // - node_ptr is the origial list's tail
+                if let Some(prev) = prev {
+                    (*node_ptr).prev = None;
+                    (*prev).next = None;
+                }
+
+                // Modify self
+                self.list.len = new_len;
+                self.list.head = new_head;
+                self.index = new_index;
+
+                // Return the new list
+                List {
+                    head: output_head,
+                    tail: output_tail,
+                    len: output_len,
+                    //_marker: PhantomData,
+                }
+            }
+        // If the cursor is on the sentinel replace the list with an empty one
+        } else {
+            std::mem::replace(self.list, List::new())
+        }
+    }
+
+    /** Returns a new list that includes all nodes after the cursor, 
+    modifying self to become all nodes before (and including) the cursor */
+    pub fn split_after(&mut self) -> List<T> {
+        // Precondition:
+        //
+        //     list.front -> A <-> B <-> C <-> D <- list.back
+        //                         ^
+        //                        cur
+        //
+        // Postcondition:
+        //
+        //     list.front -> A <-> B <- list.back
+        //                         ^
+        //                        cur
+        //
+        //    return.front -> C <-> D <- return.back
+        
+        // If the cursor is not on a sentinel node
+        if let Some(node_ptr) = self.cursor {
+            unsafe {
+                // Captures current state of self
+                let old_len = self.list.len;
+                let old_index = self.index.unwrap();
+                let cursor_next = (*node_ptr).next;
+
+                // What self will become
+                let new_len = old_index + 1;
+                let new_tail = self.cursor;
+                let new_index = Some(old_index);
+
+                // What the output will become
+                let output_len = old_len - new_len;
+                let output_head = cursor_next;
+                let output_tail = self.list.tail;
+
+                // If the cursor is NOT at the end of the list,
+                // break pointer links at split:
+                // - cursor_next is the new list's head
+                // - node_ptr is the origial list's tail
+                if let Some(next_ptr_unwrapped) = cursor_next {
+                    (*node_ptr).next = None;
+                    (*next_ptr_unwrapped).prev = None;
+                }
+
+                // Modify self
+                self.list.len = new_len;
+                self.list.tail = new_tail;
+                self.index = new_index;
+
+                // Return the new list
+                List {
+                    head: output_head,
+                    tail: output_tail,
+                    len: output_len,
+                    //_marker: PhantomData,
+                }
+            }
+        // If the cursor is on the sentinel replace the list with an empty one
+        } else {
+            std::mem::replace(self.list, List::new())
+        }
+    }
+
+    /** Splices in a new list between the cursor node and the previous node */
+    pub fn splice_before(&mut self, mut input: List<T>) {
+        // Precondition:
+        //
+        // input.front -> 1 <-> 2 <- input.back
+        //
+        // list.front -> A <-> B <-> C <- list.back
+        //                     ^
+        //                    cur
+        // Postcondition: 
+        //
+        // list.front -> A <-> 1 <-> 2 <-> B <-> C <- list.back
+        //                                 ^
+        //                                cur
+        //
+        unsafe {
+            // Per the Too Many Linked Lists Book:
+            // We can either `take` the input's pointers or `mem::forget`
+            // it. Using `take` is more responsible in case we ever do custom
+            // allocators or something that also needs to be cleaned up!
+            if input.is_empty() {
+                // Input is an empty list, easy!
+            } else if let Some(cur) = self.cursor {
+                // Both lists are non-empty
+                let input_head = input.head.take().unwrap();
+                let input_tail = input.tail.take().unwrap();
+
+                // If the cursor doesn't point to self.head
+                // 1) General case, puttin a list inside another list
+                if let Some(prev) = (*cur).prev {
+                    (*prev).next = Some(input_head);
+                    (*input_head).prev = Some(prev);
+                    (*cur).prev = Some(input_tail);
+                    (*input_tail).next = Some(cur);
+                // 2) At head, prepend self with input list
+                } else {
+                    (*cur).prev = Some(input_tail);
+                    (*input_tail).next = Some(cur);
+                    self.list.head = Some(input_head);
+                }
+                // Index moves forward by input length
+                *self.index.as_mut().unwrap() += input.len;
+            // 3) On the marker for a non-empty list, append self with input
+            } else if let Some(_back) = self.list.tail {
+                let input_head = input.head.take().unwrap();
+                let input_tail = input.tail.take().unwrap();
+
+                //(*back).prev = Some(input_head);
+                //(*in_front).next = Some(back);
+                self.list.head = Some(input_head);
+                self.list.tail = Some(input_tail);
+            } else {
+                // We're empty, become the input, remain on the ghost
+                std::mem::swap(self.list, &mut input);
+            }
+
+            self.list.len += input.len;
+            // Not necessary but Polite To Do
+            input.len = 0;
+
+            // Input dropped here
+        }
+    }
+
+    pub fn splice_after(&mut self, mut input: List<T>) {
+        // Precondition
+        //
+        // input.front -> 1 <-> 2 <- input.back
+        //
+        // list.front -> A <-> B <-> C <- list.back
+        //                     ^
+        //                    cur
+        //
+        // Postcondition
+        //
+        // list.front -> A <-> B <-> 1 <-> 2 <-> C <- list.back
+        //                     ^
+        //                    cur
+        //
+        unsafe {
+            // From the Too Many Linked List books
+            // We can either `take` the input's pointers or `mem::forget`
+            // it. Using `take` is more responsible in case we ever do custom
+            // allocators or something that also needs to be cleaned up!
+            if input.is_empty() {
+                // Input list is empty, easy!
+            // If both lists are non-empty
+            } else if let Some(cur) = self.cursor {
+                let input_head = input.head.take().unwrap();
+                let input_tail = input.tail.take().unwrap();
+
+                // 1) General case; swappin pointers ;)
+                if let Some(next) = (*cur).next {
+                    (*next).prev = Some(input_tail);
+                    (*input_tail).next = Some(next);
+                    (*cur).next = Some(input_head);
+                    (*input_head).prev = Some(cur);
+                // 2) Cursor is at tail, append self
+                } else {
+                    (*cur).next = Some(input_head);
+                    (*input_head).prev = Some(cur);
+                    self.list.tail = Some(input_tail);
+                }
+            // Index doesn't change
+            } else if let Some(head) = self.list.head {
+                // 3) Cursor is at the sentinel of a non-empty self, prepend self
+                let input_head = input.head.take().unwrap();
+                let input_tail = input.tail.take().unwrap();
+
+                (*head).next = Some(input_tail);
+                (*input_tail).prev = Some(head);
+                self.list.head = Some(input_head);
+            // At the sentinel for an empty self, do a swaperoo
+            } else {
+                std::mem::swap(self.list, &mut input);
+            }
+
+            self.list.len += input.len;
+            input.len = 0; // Unnecessasry but polite gesture
+
+            // Input dropped at end of block
+        }
+    }
 }
 
 #[cfg(test)]
@@ -542,6 +784,138 @@ mod tests {
 
     }
 
+    #[test]
+    fn split_after_test() {
+
+        use crate::lists::generic_doubly_linked_list::{List, CursorMut};
+    
+        // Creates a new doubly-linked list 
+        // and pushes some elements to it
+        let mut list = List::new();
+        list.push_head("a");
+        list.push_head("b");
+        list.push_head("c");
+        list.push_head("d");
+        list.push_head("e"); // Postcondition: [e, d, c, b, a]
+
+        let mut cur = list.cursor_mut();
+        cur.move_next(); // 0
+        cur.move_next(); // 1
+        cur.move_next(); // 2
+
+        // Split the list!
+        let mut a = cur.split_after();
+
+        // The new list's head is now b
+        let tail = a.pop_head().unwrap();
+        assert_eq!(tail, "b");
+
+        // The original list's tail is now c
+        let tail = list.pop_tail().unwrap();
+        assert_eq!(tail, "c");
+
+    }
+
+    #[test]
+    fn split_before_test() {
+
+        use crate::lists::generic_doubly_linked_list::{List, CursorMut};
+    
+        // Creates a new doubly-linked list 
+        // and pushes some elements to it
+        let mut list = List::new();
+        list.push_head("a");
+        list.push_head("b");
+        list.push_head("c");
+        list.push_head("d");
+        list.push_head("e"); // Postcondition: [e, d, c, b, a]
+
+        let mut cur = list.cursor_mut();
+        cur.move_next(); // 0
+        cur.move_next(); // 1
+        cur.move_next(); // 2
+
+        // Split the list!
+        let mut new = cur.split_before();
+
+        // The new list's head is now b
+        let tail = new.pop_tail().unwrap();
+        assert_eq!(tail, "d");
+
+        // The original list's tail is now c
+        let tail = list.pop_head().unwrap();
+        assert_eq!(tail, "c");
+
+    }
+
+    #[test]
+    fn splice_before_test() {
+
+        use crate::lists::generic_doubly_linked_list::{List, CursorMut};
+    
+        // Creates a new doubly-linked list 
+        // and pushes some elements to it
+        let mut list = List::<&str>::new();
+        list.push_tail("a");
+        list.push_tail("b");
+        list.push_tail("c"); // Postcondition: [a, b, c]
+
+        let mut cur = list.cursor_mut();
+        cur.move_next(); // a
+        cur.move_next(); // b
+        cur.move_next(); // c
+
+        let mut new_list = List::<&str>::new();
+        new_list.push_tail("1");
+        new_list.push_tail("2"); // Postcondition: [1, 2]
+
+        // Splice the list!
+        cur.splice_before(new_list); // Postcondition: [a, b, 1, 2, c]
+
+        let mut tail = list.pop_tail();
+        assert_eq!(tail, Some("c"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("2"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("1"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("b"));
+
+    }
+
+    #[test]
+    fn splice_after_test() {
+
+        use crate::lists::generic_doubly_linked_list::{List, CursorMut};
+    
+        // Creates a new doubly-linked list 
+        // and pushes some elements to it
+        let mut list = List::<&str>::new();
+        list.push_head("a");
+        list.push_head("b");
+        list.push_head("c"); // Postcondition: [c, b, a]
+
+        let mut cur = list.cursor_mut();
+        cur.move_next(); // c
+        cur.move_next(); // b
+
+        let mut new_list = List::<&str>::new();
+        new_list.push_head("1");
+        new_list.push_head("2"); // Postcondition: [2, 1]
+
+        // Splice the list!
+        cur.splice_after(new_list); // Postcondition: [c, b, 2, 1, a]
+
+        let mut tail = list.pop_tail();
+        assert_eq!(tail, Some("a"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("1"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("2"));
+        tail = list.pop_tail();
+        assert_eq!(tail, Some("b"));
+
+    }
 }
 
 /** Uses the List structure to implement a sorted podium list */
